a237245c7a420eb7b5a404651eec0de4a39755e5
Philipp Gollmer <pg@davitec.de>
2025-08-20 16:06:49 +0200

    Change dvlogs_logmodule to dv_logs

--- Configuration/Backend/Modules.php
+++ Configuration/Backend/Modules.php
@@ -3,13 +3,13 @@
 use Davitec\Dvlogs\Controller\LogModuleController;
 
 return [
-    'dvlogs_logmodule' => [
+    'dv_logs' => [
         'parent' => 'system',
         'position' => ['after' => 'log'],
         'access' => 'admin',
         'iconIdentifier' => 'ext-dv_logs_module',
         'labels' => 'LLL:EXT:dv_logs/Resources/Private/Language/locallang_mod.xlf',
-        'path' => '/module/system/dvlogs-logmodule',
+        'path' => '/module/system/dv-logs',
         'extensionName' => 'DvLogs',
         'controllerActions' => [
             LogModuleController::class => ['list', 'show', 'delete'],
